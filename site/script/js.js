d364788a7819f4c3961edb3eaeff07dd6eb3265e
Dmitry Shumilov <shum0@mail.ru>
2024-08-28 15:06:39 +0700

    операции с числами

--- site/script/js.js
+++ site/script/js.js
@@ -3,7 +3,7 @@
 mybutton = document.getElementById("myBtn");
 
 // Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -17,4 +17,25 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // Для Safari
   document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
